e8a77ef34c7d9517869b112741b78e150d9fd629
decode <coderguy2000@gmail.com>
2021-07-12 13:16:38 +0530

    initial commit

---
+++ data.js
@@ -0,0 +1,173 @@
+var cards = [
+    {
+        "id": 1,
+        "image": "https://gogocdn.net/cover/jewelpet.png",
+        "status": "closed",
+        "title":"Jwelpet Episode 18",
+        "episode":18,
+        "video": "https://storage.googleapis.com/model-cirrus-317008/XQAJYDSF55S/24a24_1626032425165088.0.mp4"
+    },
+    {
+        "id": 2,
+        "image": "https://gogocdn.net/cover/yami-shibai-9.png",
+        "status": "closed",
+        "title":"Yami Shibai 9 Episode 1",
+        "episode":1,
+        "video": "https://storage.googleapis.com/model-cirrus-317008/XQAJYDSF55S/24a24_1626032425165088.0.mp4"
+    },
+    {
+        "id": 3,
+        "image": "https://gogocdn.net/cover/getter-robo-arc.png",
+        "status": "closed",
+        "title":"Getter Robo Arc Episode 2",
+        "episode":2,
+        "video": "https://storage.googleapis.com/model-cirrus-317008/XQAJYDSF55S/24a24_1626032425165088.0.mp4"
+    },
+    {
+        "id": 4,
+        "image": "https://gogocdn.net/cover/shinigami-bocchan-to-kuro-maid.png",
+        "status": "closed",
+        "title":"Shinigami Bocchan to Kuro Maid Episode 2",
+        "episode":2,
+        "video": "https://storage.googleapis.com/model-cirrus-317008/XQAJYDSF55S/24a24_1626032425165088.0.mp4"
+    },
+    {
+        "id": 5,
+        "image": "https://gogocdn.net/cover/idolish7-third-beat.png",
+        "status": "closed",
+        "title":"IDOLish7: Third Beat! Eposde2",
+        "episode":2,
+        "video": "https://storage.googleapis.com/model-cirrus-317008/XQAJYDSF55S/24a24_1626032425165088.0.mp4"
+    },
+    {
+        "id": 6,
+        "image": "https://gogocdn.net/cover/getter-robo.png",
+        "status": "closed",
+        "title":"Getter Robo Episode 49",
+        "episode":49,
+        "video": "https://storage.googleapis.com/model-cirrus-317008/XQAJYDSF55S/24a24_1626032425165088.0.mp4"
+    },
+    {
+        "id": 1,
+        "image": "https://gogocdn.net/cover/jewelpet.png",
+        "status": "closed",
+        "title":"Jwelpet Episode 18",
+        "episode":18,
+        "video": "https://storage.googleapis.com/model-cirrus-317008/XQAJYDSF55S/24a24_1626032425165088.0.mp4"
+    },
+    {
+        "id": 2,
+        "image": "https://gogocdn.net/cover/yami-shibai-9.png",
+        "status": "closed",
+        "title":"Yami Shibai 9 Episode 1",
+        "episode":1,
+        "video": "https://storage.googleapis.com/model-cirrus-317008/XQAJYDSF55S/24a24_1626032425165088.0.mp4"
+    },
+    {
+        "id": 3,
+        "image": "https://gogocdn.net/cover/getter-robo-arc.png",
+        "status": "closed",
+        "title":"Getter Robo Arc Episode 2",
+        "episode":2,
+        "video": "https://storage.googleapis.com/model-cirrus-317008/XQAJYDSF55S/24a24_1626032425165088.0.mp4"
+    },
+    {
+        "id": 4,
+        "image": "https://gogocdn.net/cover/shinigami-bocchan-to-kuro-maid.png",
+        "status": "closed",
+        "title":"Shinigami Bocchan to Kuro Maid Episode 2",
+        "episode":2,
+        "video": "https://storage.googleapis.com/model-cirrus-317008/XQAJYDSF55S/24a24_1626032425165088.0.mp4"
+    },
+    {
+        "id": 5,
+        "image": "https://gogocdn.net/cover/idolish7-third-beat.png",
+        "status": "closed",
+        "title":"IDOLish7: Third Beat! Eposde2",
+        "episode":2,
+        "video": "https://storage.googleapis.com/model-cirrus-317008/XQAJYDSF55S/24a24_1626032425165088.0.mp4"
+    },
+    {
+        "id": 6,
+        "image": "https://gogocdn.net/cover/getter-robo.png",
+        "status": "closed",
+        "title":"Getter Robo Episode 49",
+        "episode":49,
+        "video": "https://storage.googleapis.com/model-cirrus-317008/XQAJYDSF55S/24a24_1626032425165088.0.mp4"
+    },
+    {
+        "id": 1,
+        "image": "https://gogocdn.net/cover/jewelpet.png",
+        "status": "closed",
+        "title":"Jwelpet Episode 18",
+        "episode":18,
+        "video": "https://storage.googleapis.com/model-cirrus-317008/XQAJYDSF55S/24a24_1626032425165088.0.mp4"
+    },
+    {
+        "id": 2,
+        "image": "https://gogocdn.net/cover/yami-shibai-9.png",
+        "status": "closed",
+        "title":"Yami Shibai 9 Episode 1",
+        "episode":1,
+        "video": "https://storage.googleapis.com/model-cirrus-317008/XQAJYDSF55S/24a24_1626032425165088.0.mp4"
+    },
+    {
+        "id": 3,
+        "image": "https://gogocdn.net/cover/getter-robo-arc.png",
+        "status": "closed",
+        "title":"Getter Robo Arc Episode 2",
+        "episode":2,
+        "video": "https://storage.googleapis.com/model-cirrus-317008/XQAJYDSF55S/24a24_1626032425165088.0.mp4"
+    },
+    {
+        "id": 4,
+        "image": "https://gogocdn.net/cover/shinigami-bocchan-to-kuro-maid.png",
+        "status": "closed",
+        "title":"Shinigami Bocchan to Kuro Maid Episode 2",
+        "episode":2,
+        "video": "https://storage.googleapis.com/model-cirrus-317008/XQAJYDSF55S/24a24_1626032425165088.0.mp4"
+    },
+    {
+        "id": 5,
+        "image": "https://gogocdn.net/cover/idolish7-third-beat.png",
+        "status": "closed",
+        "title":"IDOLish7: Third Beat! Eposde2",
+        "episode":2,
+        "video": "https://storage.googleapis.com/model-cirrus-317008/XQAJYDSF55S/24a24_1626032425165088.0.mp4"
+    },
+    {
+        "id": 6,
+        "image": "https://gogocdn.net/cover/getter-robo.png",
+        "status": "closed",
+        "title":"Getter Robo Episode 49",
+        "episode":49,
+        "video": "https://storage.googleapis.com/model-cirrus-317008/XQAJYDSF55S/24a24_1626032425165088.0.mp4"
+    }
+]
+
+displayCard=(data)=>{
+
+    var cardsString="";
+
+    data.forEach((card ,index)=> {
+        cardsString+=
+        `<div class="card ${card.status}" style="background-image:url(${card.image})" onclick="openCard(${index})">
+        
+        <div class="title">
+            <p>${card.title}</p>
+            <div class="button">EP${card.episode}</div>
+        </div>
+        </div>`
+    });
+    console.log(cardsString);
+    document.getElementById('cards').innerHTML=cardsString;
+}
+
+console.log(cards);
+displayCard(cards);
+
+
+openCard=(index)=>{
+    console.log(index);
+
+}
